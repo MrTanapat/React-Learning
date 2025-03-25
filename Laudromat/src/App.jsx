f02de9f8533f625347424d8ebd1433b4e052cb52
0VERT <tan.tanapat2002@gmail.com>
2025-03-25 20:40:08 +0700

    Update Navbar&Cards

--- Laudromat/src/App.jsx
+++ Laudromat/src/App.jsx
@@ -1,10 +1,13 @@
 import './index.css'
+import Navbar from '../components/navbar/nav.jsx'
+import Content from '../components/content/content.jsx';
 
 function App() {
 
   return (
     <>
-      
+      <Navbar/>
+      <Content/>
     </>
   )
 }
