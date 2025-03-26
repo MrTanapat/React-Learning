36374a153c4a7e889b420e19f4c040305aa919f2
0VERT <tan.tanapat2002@gmail.com>
2025-03-26 16:37:50 +0700

    Update Laudromat

---
+++ laudromat/Components/Content/Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Card'
+
+function Content() {
+  return (
+    <>
+    <div className="container flex flex-warp px-4 py-3 mx-auto text-slate-800">
+        <Card/>
+    </div>
+    </>
+  )
+}
+
+export default Content
