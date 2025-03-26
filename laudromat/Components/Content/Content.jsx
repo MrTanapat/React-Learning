e182644d88ecb4eab3080dac14b42e5ee4a34383
0VERT <tan.tanapat2002@gmail.com>
2025-03-26 21:14:33 +0700

    Update Card&Content

--- laudromat/Components/Content/Content.jsx
+++ laudromat/Components/Content/Content.jsx
@@ -4,7 +4,10 @@ import Card from './Card'
 function Content() {
   return (
     <>
-    <div className="container flex flex-warp px-4 py-3 mx-auto text-slate-800">
+    <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-5">
+        <Card/>
+        <Card/>
+        <Card/>
         <Card/>
     </div>
     </>
